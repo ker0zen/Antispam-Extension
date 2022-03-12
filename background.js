c77506e7a6d2168e12c3e83190987fbe46efc632
batlescouilles <jama.xapal@protonmail.com>
2022-03-12 12:12:14 +0100

    new

---
+++ background.js
@@ -0,0 +1,4 @@
+chrome.action.setBadgeText({text: 'ON'});
+chrome.action.setBadgeBackgroundColor({color: 'red'});
+
+//autofill https://github.com/bitwarden/browser/blob/master/src/content/autofill.js
