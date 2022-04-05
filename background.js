08fa4ca36e0f8b92e9f0e153d4cb5b8863bddbee
batlescouilles <jama.xapal@protonmail.com>
2022-04-05 15:13:21 +0200

    new v1

--- background.js
+++
@@ -1,7 +0,0 @@
-//autofill https://github.com/bitwarden/browser/blob/master/src/content/autofill.js
-chrome.action.onClicked.addListener((tab) => {
-  chrome.scripting.executeScript({
-    target: { tabId: tab.id },
-    files: ["script.js"],
-  });
-});
